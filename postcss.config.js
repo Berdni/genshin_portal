534864b0ce7a5510cc348a37296e50cc99331759
Konstantin Berdnikov <berdnikov_dev@vk.com>
2022-11-30 13:53:18 +0700

    Add reset styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
+  parser: 'postcss-scss',
   plugins: [
+    require('postcss-advanced-variables'),
+    require('postcss-import'),
     require('postcss-nesting'),
     require('autoprefixer'),
   ],
