a32b0efda3e484059f7537a2ca766b85210b879e
Konstantin Berdnikov <berdnikov_dev@vk.com>
2022-12-05 11:02:39 +0700

    Add administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   devise_for :users
   root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
